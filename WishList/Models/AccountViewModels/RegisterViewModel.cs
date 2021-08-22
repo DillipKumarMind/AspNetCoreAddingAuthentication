fe54914a744ec0d4d7d1551527099e8675546610
Shahrukh Alam <srk.cps12345@gmail.com>
2021-08-23 01:00:10 +0530

    Update RegisterViewModel.cs

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -10,10 +10,15 @@ namespace WishList.Models.AccountViewModels
     public class RegisterViewModel : Controller
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
-        [Required, StringLength(100), MinLength(8)]
+        [Required]
+        [StringLength(100,MinimumLength =8)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
     }
 }
