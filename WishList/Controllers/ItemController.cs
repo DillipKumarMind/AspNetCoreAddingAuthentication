087afd80a787850462c4dbddf2990f3aaa5da384
Shahrukh Alam <srk.cps12345@gmail.com>
2021-08-23 11:18:34 +0530

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -20,8 +20,8 @@ namespace WishList.Controllers
 
         public IActionResult Index()
         {
-            var model = _context.Items.ToList();
-
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            var model = _context.Items.Where(e => e.User.Id == user.Id).ToList();
             return View("Index", model);
         }
 
@@ -34,6 +34,8 @@ namespace WishList.Controllers
         [HttpPost]
         public IActionResult Create(Models.Item item)
         {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            item.User = user;
             _context.Items.Add(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
@@ -41,7 +43,10 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            if (item.User.Id != user.Id)
+                return Unauthorized();
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
